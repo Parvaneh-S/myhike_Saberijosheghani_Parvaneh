db1b9ab32580461edf821afeacf36b8b35435525
Parvaneh-S <psaberijosheghani@my.bcit.ca>
2024-11-05 12:37:50 -0800

    Nov5-demo10

--- scripts/firebaseAPI_template.js
+++ scripts/firebaseAPI_template.js
@@ -1,14 +1,14 @@
 //----------------------------------------
 //  Your web app's Firebase configuration
 //----------------------------------------
-var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyASHSt-KLdTzAFu-YxerxclST4QBoq2ISI",
+    authDomain: "comp1800-202430-demo-6031e.firebaseapp.com",
+    projectId: "comp1800-202430-demo-6031e",
+    storageBucket: "comp1800-202430-demo-6031e.firebasestorage.app",
+    messagingSenderId: "30857999342",
+    appId: "1:30857999342:web:52de8395ba042a531b94ff"
+  };
 
 //--------------------------------------------
 // initialize the Firebase app
